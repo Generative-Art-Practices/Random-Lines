a0979d7d300591b17adcfad9f469f123141af393
thisisabhinay <a8h1nay@gmail.com>
2019-12-13 22:05:54 +0530

    Adds code of the art

---
+++ src/script.js
@@ -0,0 +1,23 @@
+function setup () {
+  createCanvas(windowWidth, windowHeight);
+  background("#C42847");
+  noLoop();
+}
+
+function draw () {
+  const maxWidth = windowWidth;
+  for (let i = 0; i < 80; i++){
+    // Configuring this stroke.
+    let randomNumber = random(maxWidth);
+    let randomWidth = random(10);
+    stroke('#2C2C54');
+    strokeWeight(randomWidth);
+    
+    // Creating this stroke.
+    line(0, i * 20, 10 + randomNumber, 0);
+    
+    // let cnv = createCanvas(windowWidth, windowHeight);
+    // save(cnv, 'myCanvas.jpg');
+  }
+}
+
